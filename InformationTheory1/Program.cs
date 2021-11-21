e9b2cd7658932fc206f84690b0b44ccdb33c046a
Deividas Zaleskis <deividas.zaleskis@devbridge.com>
2021-11-21 19:30:52 +0200

    init commit

---
+++ InformationTheory1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InformationTheory1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
